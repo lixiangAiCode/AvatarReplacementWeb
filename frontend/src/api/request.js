973ac9ed12d2ae3770762089239fa6476eb10f35
lixiangAiCode <llf334998@gmail.com>
2025-09-26 13:29:45 +0800

    更新前端API地址为新的Cloudflare隧道域名

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://convicted-camp-physiology-invention.trycloudflare.com/api',
+  baseURL: 'https://institute-chelsea-schedules-think.trycloudflare.com/api',
   timeout: 30000
 })
 
